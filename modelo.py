1f990bfbb9446a461e0f3b93c1da30a75b3ad081
Renato Luiz de Almeida <38532053+Renato9889@users.noreply.github.com>
2022-04-26 17:34:51 -0300

    Add files via upload

---
+++ modelo.py
@@ -0,0 +1,44 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self,novo_nome):
+        self._nome = novo_nome.title()
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome,ano)
+        self.duracao = duracao
+
+    def __str__(self):
+        return f"Nome: {self._nome} - Ano: {self.ano} - Duração: {self.duracao} - Likes: {self.likes}"
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome,ano)
+        self.temporadas = temporadas
+
+    def __str__(self):
+        return f"Nome: {self._nome} - Ano: {self.ano} - Temporadas: {self.temporadas} - Likes: {self.likes}"
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+
+    def tamanho(self):
+        return len(self.programas)
---
+++ testes.py
@@ -0,0 +1,67 @@
+from modelo import Filme
+from modelo import Serie
+from modelo import Playlist
+
+filme1 = Filme("X-men - dias de um futuro esquecido", 2015,160)
+
+print(filme1)
+
+serie1 = Serie("love victor",2020,2)
+
+print(serie1)
+
+filme1.dar_like()
+filme1.dar_like()
+filme1.dar_like()
+
+serie1.dar_like()
+serie1.dar_like()
+
+print(filme1)
+
+print(serie1)
+
+
+serie1.nome = "Love, Victor"
+
+print(serie1)
+
+serie2 = Serie("Heartstopper",2022,1)
+serie2.dar_like()
+serie2.dar_like()
+serie2.dar_like()
+print(serie2)
+
+filmes_e_series = [filme1,serie2,serie1]
+
+print("LUPOEEEE")
+for programa in filmes_e_series:
+    print(programa)
+
+
+print("Playlist")
+
+filme2 = Filme("Vingadores - Guerra Infinita",2018,200)
+filme3 = Filme("Interestelar",2013,190)
+serie3 = Serie("WandaVision", 2020,1)
+
+serie3.dar_like()
+serie3.dar_like()
+serie3.dar_like()
+serie3.dar_like()
+serie3.dar_like()
+serie3.dar_like()
+filme2.dar_like()
+filme3.dar_like()
+filme2.dar_like()
+filme3.dar_like()
+filme2.dar_like()
+filme3.dar_like()
+filme3.dar_like()
+
+filmes_e_series = [filme1, filme2, filme3, serie1, serie2, serie3]
+filmes_favs = Playlist("Favoritos",filmes_e_series)
+
+print("Aqui:")
+for programa in filmes_favs.programas:
+    print(programa)
